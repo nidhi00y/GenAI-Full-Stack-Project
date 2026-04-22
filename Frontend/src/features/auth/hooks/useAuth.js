b696b1641c593ca6ae6369c43a7e5f33c7ff6b8c
Nidhi <nidhiy2810@gmail.com>
2026-04-22 13:32:53 +0530

    Use auth hook created

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,49 @@
+import {useContext} from 'react'
+import { AuthContext } from '../services/auth.context.jsx'
+import {login,register,logout,getCurrentUser} from '../services/auth.service.js'
+
+export function useAuth(){
+    const context = useContext(AuthContext);
+    const {user, setUser, loading, setLoading} = context;
+
+    const handleLogin = async ({email,password}) =>{
+        setLoading(true);
+        try{
+            const response = await login({email,password});
+            setUser(response.user);
+        }catch(error){
+            console.error('Login error:', error);
+        }finally{
+            setLoading(false);
+        }
+        
+    }
+
+    const handleRegister = async ({name,email,password}) =>{
+        setLoading(true);
+        try{
+            const response = await register({name,email,password});
+            setUser(response.user);
+        }catch(error){
+            console.error('Registration error:', error);
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    const handleLogout = async () =>{
+        setLoading(true);
+        try{
+            await logout();
+            setUser(null);
+        }catch(error){
+            console.error('Logout error:', error);
+        }finally{
+            setLoading(false);
+        }
+        
+    }
+
+    return {user,loading,handleLogin,handleRegister,handleLogout}
+
+}
--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import {React,useSate} from 'react'
 import './login.form.scss'
 import { useNavigate } from 'react-router';
+import { useAuth } from '../services/auth.context.jsx';
 
 
 const Login = () => {
+
+    const {loading,handleLogin} = useAuth();
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        handleLogin({email,password});
+    }
+
+    if(loading){
+        return <div><h1>Loading....</h1></div>
     }
 
     return (
@@ -18,11 +29,11 @@ const Login = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" required />
+                        <input onChange={(e)=>{setEmail(e.target.value)}} type="email" id="email" name="email" required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required />
+                        <input onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" name="password" required />
                     </div>
                     <button className='button primary-button' type="submit">Login</button>
                 </form>

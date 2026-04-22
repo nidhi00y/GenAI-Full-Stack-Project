277e4bcc2ec30b5acd85eacbe908ea88cd90725e
Nidhi <nidhiy2810@gmail.com>
2026-04-22 12:52:52 +0530

    Axios done

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: true
+});
+
+export async function register({name,emil,password}){
+    try {
+        const response = await api.post('/user/register', {
+            name,
+            email,
+            password
+        });
+        return response.data;
+    }
+    catch (error) {
+        console.error('Registration error:', error);
+    }
+}
+
+export async function login({email,password}){
+    try {
+        const response = await api.post('/user/login', {
+            email,
+            password
+        });
+        return response.data;
+    }
+    catch (error) {
+        console.error('Login error:', error);
+    }
+}
+
+export async function logout(){
+    try {
+        const response = await api.get('/user/logout', {});
+        return response.data;
+    }
+    catch (error) {
+        console.error('Logout error:', error);
+    }
+}
+
+export async function getCurrentUser(){
+    try {
+        const response = await axios.get('/user/getme', {});
+        return response.data;
+    }
+    catch (error) {
+        console.error('Get current user error:', error);
+    }
+}

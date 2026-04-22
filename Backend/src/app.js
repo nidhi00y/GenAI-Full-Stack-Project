452004f60c77c025046daf0358960c5fef7e2184
Nidhi <nidhiy2810@gmail.com>
2026-04-22 13:35:24 +0530

    cors setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import userRouter from './routes/user.route.js';
 import cookies from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookies());
 
